95f40ffdff171eedef28857e390ab49a45259308
Safayet Mahid <safayetmahid96@gmail.com>
2022-12-20 18:25:46 +0600

    initialized

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".product-slider").slick({
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".left",
+        nextArrow:".right",
+        autoplay:true,
+    })
+
+})
