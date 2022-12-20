22d5b223737397b3cf419990e5f4a5fc6eaf7dbe
Safayet Mahid <safayetmahid96@gmail.com>
2022-12-20 21:53:58 +0600

    mobile and tablet responsive done

--- js/app.js
+++ js/app.js
@@ -5,14 +5,24 @@ $(document).ready(function () {
         prevArrow: ".left",
         nextArrow: ".right",
         autoplay: true,
-        mobileFirst: true,
+        // mobileFirst: true,
         responsive: [
             {
                 breakpoint: 500,
                 settings: {
                     slidesToShow: 1,
                 }
-            }, {
+            },
+
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 2,
+
+                }
+            },
+
+            {
                 breakpoint: 1000,
                 settings: {
                     slidesToShow: 3,
